caaff132a324090aeb0bd2fd41131e2f961b7988
qiblatainf <qiblatain@live.com>
2023-06-07 17:42:33 +0500

    loaded iris data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+from sklearn.datasets import load_iris
 
 # Load the saved model
 model = joblib.load('iris_model.pkl')
@@ -10,6 +11,7 @@ app = Flask(__name__)
 def predict():
     # Get the data from the request
     data = request.get_json()
+    iris_data = load_iris()
 
     # Convert the data into a format compatible with the model
     features = [data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']]
@@ -20,7 +22,7 @@ def predict():
     predicted_class = int(prediction[0])
 
     # Return the prediction as JSON response
-    species = data.target_names[predicted_class]
+    species = iris_data.target_names[predicted_class]
     return jsonify({'species': species})
 
 if __name__ == '__main__':
